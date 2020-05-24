a252aab0d80b6d685f9c79d3cbcb6a1a417472ff
pemaddm <64878840+pemaddm@users.noreply.github.com>
2020-05-24 12:56:58 +1000

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Assignment_2E
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstUnsorted = new System.Windows.Forms.ListBox();
+            this.lstSorted = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(516, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File and Sort:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstUnsorted
+            // 
+            this.lstUnsorted.FormattingEnabled = true;
+            this.lstUnsorted.ItemHeight = 16;
+            this.lstUnsorted.Location = new System.Drawing.Point(87, 162);
+            this.lstUnsorted.Name = "lstUnsorted";
+            this.lstUnsorted.Size = new System.Drawing.Size(209, 228);
+            this.lstUnsorted.TabIndex = 1;
+            // 
+            // lstSorted
+            // 
+            this.lstSorted.FormattingEnabled = true;
+            this.lstSorted.ItemHeight = 16;
+            this.lstSorted.Location = new System.Drawing.Point(465, 162);
+            this.lstSorted.Name = "lstSorted";
+            this.lstSorted.Size = new System.Drawing.Size(209, 228);
+            this.lstSorted.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Array Before Sorting";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(462, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Array After Sorting";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(309, 402);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(158, 36);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstSorted);
+            this.Controls.Add(this.lstUnsorted);
+            this.Controls.Add(this.button1);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assignment 2E";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstUnsorted;
+        private System.Windows.Forms.ListBox lstSorted;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,78 @@
+﻿//*******************************************************************
+// Programmer: Pema Thinley
+// Date: 23 May 2020
+// Software: Microsoft Visual Studio 2019 Community Edition
+// Platform: Microsoft Windows 10 Professional 64bit Version 1803
+// Purpose: Code for Sorting using Comparator
+//*******************************************************************
+
+using System;
+using System.Collections; 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2E
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        string fileName;
+        const int rows = 4;  //constant variable to hold number of rows
+        const int cols = 2;  //constant variable to hold number of columns
+        int[,] finesArray = new int[rows, cols]; //2D array to hold the fines read from file
+        int[] sortArray = new int[rows]; //1D array to hold the fines
+        private StreamReader fileReader;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string readfile; // reading row 
+            dlgOpen.Filter = "Text files (*.txt)|*.txt"; //setting filter to allow only text file
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+                fileName = dlgOpen.FileName;
+            FileStream input = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            //Set file from where data is read
+            fileReader = new StreamReader(input);
+
+            for (int i = 0; i < rows; i++)
+            {
+                readfile = fileReader.ReadLine();// read a recod from the file
+                sortArray[i] = Convert.ToInt32(readfile);
+                lstUnsorted.Items.Add(sortArray[i]); //add content of the array to the unsorted list
+
+            }
+
+            //For sorting the array using Sort(Array, IComparer) Method
+            IComparer cmp = new compare();
+            Array.Sort(sortArray, cmp);
+            for (int i = 0; i < rows; i++)
+            {
+                lstSorted.Items.Add(sortArray[i]); //Add content of the array to sorted list
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    class compare : IComparer //Comparator class
+    {
+        // Call CaseInsensitiveComparer.Compare 
+        public int Compare(Object x, Object y)
+        {
+            return (new CaseInsensitiveComparer()).Compare(x, y);
+        }
+    }
+}
